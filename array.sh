e6887736d098ea912e398bdac86ae17e0c981c16
Um e Habiba <uhabiba005@gmail.com>
2023-10-02 20:36:49 +0200

    Add array.sh

---
+++ array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MY_FIRST_ARRAY=(one two three four five)
+
+echo ${MY_FIRST_ARRAY[@]} # Printing all elements of array
+echo ${MY_FIRST_ARRAY[0]} # Printing a element of array
--- shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ${1,,} = habiba ]; then # ,, converts the string to lowercase
-    echo "Habiba is here"
-elif [ ${1,,} = help ]; then
-    echo "How can I help you, if you cant help yourself"
-else
-    echo "Habiba? 404 Not Found"
-fi
