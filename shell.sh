4592708bd736e40f14ea8892f11236ec38107021
Um e Habiba <uhabiba005@gmail.com>
2023-10-02 17:17:05 +0200

    Add Shell Parameter Expansion section to doc

--- shell.sh
+++ shell.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo Hello, $1
+if [ ${1,,} = habiba ]; then # ,, converts the string to lowercase
+    echo "Habiba is here"
+elif [ ${1,,} = help ]; then
+    echo "How can I help you, if you cant help yourself"
+else
+    echo "Habiba? 404 Not Found"
+fi
