0f49062f3b135b54850b1f12362c2747aefe3f93
Ume0344 <uhabiba005@gmail.com>
2023-10-03 00:06:19 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-MY_FIRST_ARRAY=(one two three four five)
+MY_FIRST_ARRAY=(one two three four five) # a global variable
 
 function for_loop {
+    local MY_FIRST_ARRAY=(1 2 3 4 5) # a local variable
     for item in ${MY_FIRST_ARRAY[@]}; do
     echo $item 
     echo -n $item | wc -c; # -n donot insert a new line at end of each item
 done
+}
+
+function with_arguments {
+    echo Hello $1
 }
 
 show_uptime() {
@@ -21,3 +26,4 @@ EOF
 
 for_loop
 show_uptime
+with_arguments Habiba
