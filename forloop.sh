d59e13b31acb982d22821889f60a68e31c388677
Ume0344 <uhabiba005@gmail.com>
2023-10-03 20:45:24 +0200

    Update forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -6,3 +6,30 @@ for item in ${MY_FIRST_ARRAY[@]}; do
     echo $item 
     echo -n $item | wc -c; # -n donot insert a new line at end of each item
 done
+
+for x in www.google.com www.facebook.com www.singing.com;
+do 
+    #/dev/null is the virtual device, and output of ping will stored in it. If ping is successful
+    # the output is 0 stored in /dev/null
+    # c flag is count of ping, w flag is for wait in miliseconds.
+    if ping -q -c 2 -w 1 $x > /dev/null; then
+        echo "$x is up"
+    else
+        echo "$x is down"
+    fi
+done
+
+for x in $(cat test.txt);
+do
+    echo $x
+done
+
+# continue and break
+
+for x in {1..20};
+do
+    if [$x=10]; then
+        break
+    echo $x
+    fi
+done
